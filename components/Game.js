5a2974ae2ce166d7fd4a5395e1a359a90f7a3868
Samika11 <79766127+Samika11@users.noreply.github.com>
2023-07-06 13:57:27 -0400

    Update Game.js

--- components/Game.js
+++ components/Game.js
@@ -41,15 +41,44 @@ AFRAME.registerComponent("game-play", {
       }
     }
   },
-  isCollided: function (elemntId) {
-    const element = document.querySelector(elemntId);
+  isCollided: function (elementId) {
+    const element = document.querySelector(elementId);
     element.addEventListener("collide", (e) => {
-      if (elemntId.includes("#ring")) {
+      if (elementId.includes("#ring")) {
+        element.setAttribute("visible",false);
+        this.updateScore();
+        this.updateTargets();
         
       } else {
-        
+       this.gameover(); 
       }
     });
   },
+  updateTargets:function(){
+    var element=document.querySelector('#targets');
+    var count=element.getAttribute("text").value;
+    var currentTargets=parseInt(count);
+    currentTargets-=1;
+    element.setAttribute("text",{
+      value:currentTargets
+    })
+  },
+  updateScore:function(){
+    var element=document.querySelector("#score");
+    var count=element.getAttribute("text").value;
+    var currentScore=parseInt(count);
+    currentScore+=50;
+    element.setAttribute("text",{
+      value:currentScore
+    })
+  },
+  gameover:function(){
+    var planeEl=document.querySelector("#plane_model");
+    var element=document.querySelector("#game_over_text");
+    element.setAttribute("visible",true);
+    planeEl.setAttribute("dynamic-body",{
+      mass:1
+    })
+  }
   
 });
